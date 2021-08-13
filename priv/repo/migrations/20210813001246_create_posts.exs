7b4b9bf1e69c78f40cf79c6079d7ec5a745d566d
José Camelo <josecfreittas@outlook.com>
2021-08-13 12:42:33 -0300

    Cascade delete all the user posts when the user is deleted

--- priv/repo/migrations/20210813001246_create_posts.exs
+++ priv/repo/migrations/20210813001246_create_posts.exs
@@ -5,7 +5,7 @@ defmodule Exblog.Repo.Migrations.CreatePosts do
     create table(:posts) do
       add :title, :string
       add :content, :text
-      add :user_id, references(:users, on_delete: :nothing)
+      add :user_id, references(:users, on_delete: :delete_all)
 
       timestamps()
     end
