e54ff22ce20c9f9e5d3ecb68c0054a6084710db5
José Camelo de Freitas <josecfreittas@outlook.com>
2021-09-21 00:41:03 -0300

    Cleanup ExblogWeb module

--- lib/exblog_web.ex
+++ lib/exblog_web.ex
@@ -1,22 +1,4 @@
 defmodule ExblogWeb do
-  @moduledoc """
-  The entrypoint for defining your web interface, such
-  as controllers, views, channels and so on.
-
-  This can be used in your application as:
-
-      use ExblogWeb, :controller
-      use ExblogWeb, :view
-
-  The definitions below will be executed for every view,
-  controller, etc, so keep them short and clean, focused
-  on imports, uses and aliases.
-
-  Do NOT define functions inside the quoted expressions
-  below. Instead, define any helper function in modules
-  and import those modules here.
-  """
-
   def controller do
     quote do
       use Phoenix.Controller, namespace: ExblogWeb
@@ -47,12 +29,6 @@ defmodule ExblogWeb do
 
       import Plug.Conn
       import Phoenix.Controller
-    end
-  end
-
-  def channel do
-    quote do
-      use Phoenix.Channel
     end
   end
 
